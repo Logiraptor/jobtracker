8212b0c4287d9f416e5ff699c7e41c021ab26dc9
Patrick Oyarzun <patrickoyarzun@gmail.com>
2016-06-10 22:00:26 -0400

    Added Generate pdf link

--- main.go
+++ main.go
@@ -1,15 +1,44 @@
 package main
 
 import (
-	"fmt"
+	"encoding/hex"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
+	"os/exec"
+	"path/filepath"
 )
 
 func main() {
 	http.HandleFunc("/", func(rw http.ResponseWriter, req *http.Request) {
-		fmt.Fprint(rw, "Welcome to Job Tracker")
+		http.ServeFile(rw, req, "public/index.html")
+	})
+
+	http.HandleFunc("/pdf", func(rw http.ResponseWriter, req *http.Request) {
+		var buf = make([]byte, 32)
+		rand.Read(buf)
+		randomString := hex.EncodeToString(buf)
+		f, err := os.Create(filepath.Join(os.TempDir(), randomString+".html"))
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		f.WriteString("<html></html>")
+		f.Close()
+
+		pdfName := f.Name() + ".pdf"
+
+		log.Println("wkhtmltopdf", f.Name(), pdfName)
+		output, err := exec.Command("wkhtmltopdf", f.Name(), pdfName).CombinedOutput()
+		log.Println(string(output))
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		http.ServeFile(rw, req, pdfName)
+		os.Remove(f.Name())
+		os.Remove(pdfName)
 	})
 
 	port := os.Getenv("PORT")
